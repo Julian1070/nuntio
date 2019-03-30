1dcdc8f376222e04921d016feef4fb67f1b15f25
Julian <julianmgrosse@gmail.com>
2019-03-30 16:57:13 -0700

    Updates

---
+++ precache-manifest.2983299bf82f8f8b1d60b3f908586381.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a836031d43429e189f87",
+    "url": "/nuntio/static/css/main.657f0bc3.chunk.css"
+  },
+  {
+    "revision": "a836031d43429e189f87",
+    "url": "/nuntio/static/js/main.7c3b7dbc.chunk.js"
+  },
+  {
+    "revision": "d5336db5d415b1db3c98",
+    "url": "/nuntio/static/js/runtime~main.75fbe88c.js"
+  },
+  {
+    "revision": "f1a7162316f86bac9d76",
+    "url": "/nuntio/static/js/2.07f6d3e1.chunk.js"
+  },
+  {
+    "revision": "80da24e4fcaf621aad3236c53360840c",
+    "url": "/nuntio/static/media/correct.80da24e4.mp3"
+  },
+  {
+    "revision": "248b888b412e6ecb8d6aa5b44ee63670",
+    "url": "/nuntio/static/media/incorrect.248b888b.mp3"
+  },
+  {
+    "revision": "8cdd7f48d074d93c1563d93c252b777e",
+    "url": "/nuntio/index.html"
+  }
+];
